594db94effb7cb18a65e9ad6b0a5d0f5fc1d05fa
Wurikos <50768680+Wurikos@users.noreply.github.com>
2021-02-05 10:13:03 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"601be5e88954e5001ef1457a",
+				"601be5e88954e5001ef1457c"
+			]
+		},
+		"601be5e88954e5001ef1457a": {
+			"id": "601be5e88954e5001ef1457a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"601be5e88954e5001ef1457c": {
+			"id": "601be5e88954e5001ef1457c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as Testcomp } from "./Testcomp"
